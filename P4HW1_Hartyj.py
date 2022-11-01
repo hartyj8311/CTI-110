3f5bc54ff046f3501abf1d7bede51f89d9c83d53
hartyj8311 <114525147+hartyj8311@users.noreply.github.com>
2022-11-01 14:09:30 -0400

    Update P4HW1_Hartyj.py

--- P4HW1_Hartyj.py
+++ P4HW1_Hartyj.py
@@ -6,4 +6,23 @@
 # Ask the user for 6 grades for the 6 modules
 # Add them to a list.
 
-grades = [0, 0, 0, 0, 0, 0]
+grades = []
+
+# grades = int(input("Enter grade for Module1:")
+# grades[0] = grade1
+
+for grade in range(6):
+    grade = int(input("Enter grade: "))
+    grades.append(grade)
+
+print("The grades are: ", grades)
+print("Highest grade: ", max(grades))
+print("Lowest grade: ", min(grades))
+
+# Now the average - devide the sum by the length (count)
+total = sum(grades)
+count = len(grades)
+average = total / count
+print("Total is: ", total)
+print("Average is: ", average)
+             
